b4c3198b1f669416ec64af1355569ad4ef920977
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-05-27 11:59:18 +0200

    added header for forward declarations

---
+++ include/gbAudio/AudioFwd.hpp
@@ -0,0 +1,18 @@
+#ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_AUDIO_FWD_HPP
+#define GHULBUS_LIBRARY_INCLUDE_GUARD_AUDIO_FWD_HPP
+
+/** @file
+*
+* @brief Forward declarations for gbAudio.
+* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+*/
+
+#include <gbAudio/config.hpp>
+#include <memory>
+
+namespace GHULBUS_AUDIO_NAMESPACE
+{
+    class AudioDevice;
+    typedef std::unique_ptr<AudioDevice> AudioDevicePtr;
+}
+#endif
